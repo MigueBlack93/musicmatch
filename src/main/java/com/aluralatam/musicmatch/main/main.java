e959794cdd85998393d9544bcf4cb7d6f398a7db
Migue <miguelulmi@hotmail.com>
2024-10-25 11:17:34 -0300

    Update main.java

--- src/main/java/com/aluralatam/musicmatch/main/main.java
+++ src/main/java/com/aluralatam/musicmatch/main/main.java
@@ -1,4 +1,7 @@
 package com.aluralatam.musicmatch.main;
 
 public class main {
+    private String urlCancionesPorAlbum = "www.theaudiodb.com/api/v1/json/2/track.php?m=";//Agregar ID Album
+    private String urlArtista = "https://www.theaudiodb.com/api/v1/json/2/searchalbum.php?s=";//Agregar nombre artirsta low case separado por guión bajo
+
 }
